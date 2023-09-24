b631a0de9cfd477cf960c79986f6ef4ffc888b1b
Youkwhan <Youkwhan@gmail.com>
2023-09-23 21:20:58 -0400

    feat(Navbar): Navbar responsiveness and add menu toggle This commit improves the Navbar component with the following changes: - Utilizes the  Hook to make the Navbar responsive with tailwindcss, enabling dynamic rendering of content based on screen size. - Introduces the  state to manage the mobile menu toggle functionality. - Enhances the Navbar layout and content presentation for medium to large screens. - Implements a mobile menu button for small screens to toggle the menu.

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -12,6 +13,8 @@ type Props = {
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   // reusable tailwind classes
   const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)"); // boolean
 
   return (
     <nav>
@@ -22,33 +25,43 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             <img alt="Church logo" src={Logo} />
 
             {/* RIGHT SIDE */}
-            <div className={`${flexBetween} w-full`}>
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="About"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Our Service"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Contact Us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="About"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Service"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <button>Join Us Sunday</button>
+                </div>
               </div>
-              <div className={`${flexBetween} gap-8`}>
-                <button>Join Us Sunday</button>
-              </div>
-            </div>
+            ) : (
+              // MENU BAR (mobile v.)
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
         </div>
       </div>
