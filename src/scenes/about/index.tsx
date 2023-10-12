fa58ff9f6bb941b47019018eeecd8be9338f8247
Youkwhan <Youkwhan@gmail.com>
2023-10-11 23:22:40 -0400

    feat(About): add animation to component header

--- src/scenes/about/index.tsx
+++ src/scenes/about/index.tsx
@@ -42,12 +42,22 @@ function About({ setSelectedPage }: Props) {
       {/* onViewportEnter: For highlighting where we are on the navigation */}
       <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.About)}>
         {/* HEADER */}
-        <div className="my-5 md:w-3/5">
+        <motion.div
+          className="my-5 md:w-3/5 "
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <HText>MORE THAN JUST A CHURCH.</HText>
           <p className="my-5 text-sm">
             Experience Grace, Community, and Spiritual Growth.
           </p>
-        </div>
+        </motion.div>
         {/* INFO Cards */}
         <motion.div
           className="mt-5 items-center justify-between gap-8 md:flex"
