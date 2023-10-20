4c3d293e76c824fb91f82328c194250d98c688b3
Youkwhan <Youkwhan@gmail.com>
2023-10-19 22:15:47 -0400

    feat(ContactUs): Finished Form with react-hook-form Validation and Submission
    This commit integrates react-hook-form for robust validation and form submission. Key features include:
    
    - Configured a form to capture user details using react-hook-form.
    - Added validation rules for name, email, and message fields.
    - Managed error messages for each field based on validation results.
    - Implemented form submission action to open a new tab using formsubmit.co.
    
    The validation rules ensure that user input adheres to specific criteria, such as required fields, maximum lengths, and email format. Error messages are displayed when validation rules are not met, providing user-friendly feedback.
    
    This feature enables users to submit inquiries, messages, or requests seamlessly, enhancing the church's accessibility and engagement with its community.

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -7,6 +7,8 @@ import HText from "@/shared/HText";
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
 function ContactUs({ setSelectedPage }: Props) {
+  const inputStyles = `mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
+
   const {
     register,
     trigger,
@@ -67,7 +69,59 @@ function ContactUs({ setSelectedPage }: Props) {
               action="https://formsubmit.co/Youkwhan@gmail.com"
               method="POST"
             >
-              <input />
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="NAME"
+                {...register("name", { required: true, maxLength: 100 })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length is 100 characters."}
+                </p>
+              )}
+
+              <input
+                className={inputStyles}
+                type="email"
+                placeholder="EMAIL"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+
+              <textarea
+                className={inputStyles}
+                rows={4}
+                cols={50}
+                placeholder="MESSAGE"
+                {...register("message", { required: true, maxLength: 2000 })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 characters."}
+                </p>
+              )}
+
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+              >
+                SUBMIT
+              </button>
             </form>
           </motion.div>
           <img />
